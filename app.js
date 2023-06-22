84ddf462d3969cb7825107dafa86285ed6119602
ryvillan <64065478+ryvillan@users.noreply.github.com>
2023-06-22 19:56:54 +0530

    handled errors

--- app.js
+++ app.js
@@ -14,20 +14,34 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     const city = req.body.cityName;
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID="+process.env.APIKEY +"&units=metric";
-    https.get(url, (response) => {
-        response.on("data", (data) => {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const wDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + process.env.APIKEY + "&units=metric";
 
-            res.render("index", { temprature: temp, icon: icon, imageLink: imageURL, description: wDescription, cityName:city });
-        })
-    })
-    
-})
+    const request = https.get(url, (response) => {
+        let data = "";
+
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+
+        response.on("end", () => {
+            try {
+                const weatherData = JSON.parse(data);
+                const temp = weatherData.main.temp;
+                const wDescription = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  
+                res.render("index", { temprature: temp, icon: icon, imageLink: imageURL, description: wDescription, cityName: city });
+            } catch (error) {
+                res.render("error", { cityName: req.body.cityName});
+            }
+        });
+    });
+
+    request.on("error", (error) => {
+        res.render("error", { errorMessage: "Error connecting to weather API" });
+    });
+});
 app.listen(3000, () => {
     console.log(" server is running");
 })
